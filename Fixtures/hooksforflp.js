75c9eede7f1b9d6220bab108f9eb2c8cf85623fa
Iyyappan Ulaganathan <iulaganathan@nextsphere.com>
2026-05-18 17:01:38 +0530

    URL Changed for Safety

--- Fixtures/hooksforflp.js
+++ Fixtures/hooksforflp.js
@@ -1,7 +1,7 @@
 import {test} from '@playwright/test';
 
 test.beforeEach(async ({page})=>{
-await page.goto("https://flptitan.com");
+await page.goto("https://demosite.com");
 console.log("LAUNCHED url..")
 })
 
